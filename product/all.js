d1d0937ea69504ac5780fa372874d1065413cc73
himanshi001 <31383458+himanshi001@users.noreply.github.com>
2023-09-29 11:21:56 +0530

    Update via AirCode on Sep 29, 2023

---
+++ product/all.js
@@ -0,0 +1,20 @@
+// @see https://docs.aircode.io/guide/functions/
+// @see https://docs.aircode.io/guide/functions/
+const aircode = require('aircode');
+
+module.exports = async function (params, context) {
+  const productTable = aircode.db.table('product');
+  
+  const products = await productTable
+  .where()
+  .find();
+
+  const count = await productTable
+  .where()
+  .count();
+
+  return {
+    count,
+    products
+  }
+};
